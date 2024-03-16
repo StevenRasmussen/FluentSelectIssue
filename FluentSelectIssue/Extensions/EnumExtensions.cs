dd64215c0cf70c6f244f0aaedb08bd0381c37706
Steven Rasmussen <steven.rasmussen@gmail.com>
2024-03-16 08:55:57 -0600

    Add project files.

---
+++ FluentSelectIssue/Extensions/EnumExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace FluentSelectIssue.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayString<T>(this T value) where T : struct, Enum
+        {
+            var enumName = value.ToString();
+            var enumType = typeof(T);
+            var memberInfos = enumType.GetMember(enumName);
+            var enumValueMemberInfo = memberInfos.FirstOrDefault(m => m.DeclaringType == enumType);
+            if (enumValueMemberInfo != null)
+            {
+                var valueAttribute = enumValueMemberInfo.GetCustomAttribute<DescriptionAttribute>();
+                if (valueAttribute is not null)
+                {
+                    return valueAttribute.Description;
+                }
+            }
+
+            return enumName.SplitCamelCase() ?? enumName;
+        }
+
+        public static string? SplitCamelCase(this string? input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+            return Regex.Replace(input, "(?<!\\b[A-Z])([A-Z])", " $1", RegexOptions.Compiled).Trim();
+        }
+    }
+}
